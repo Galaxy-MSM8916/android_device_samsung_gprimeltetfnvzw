bc05b7413b312b9cee748c5a488b91c637d70658
Vincent Zvikaramba <vincent.zvikaramba@mail.utoronto.ca>
2017-01-20 22:48:12 +0000

    gprimelte-common: Rebrand

--- lineage.mk
+++ lineage.mk
@@ -1,11 +1,11 @@
 # Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/cm.mk)
+$(call inherit-product, device/samsung/gprimelte-common/lineage.mk)
 
 $(call inherit-product, device/samsung/gprimeltetmo/full_gprimeltetmo.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gprimeltetmo
-PRODUCT_NAME := cm_gprimeltetmo
+PRODUCT_NAME := lineage_gprimeltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G530T
 PRODUCT_MANUFACTURER := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
 for i in eng userdebug user; do
-#	add_lunch_combo omni_gprimeltetmo-${i}
-	add_lunch_combo cm_gprimeltetmo-${i}
+	add_lunch_combo lineage_gprimeltetmo-${i}
 done
