2122239a14bc43723205d91c083781e5cde945a7
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-05 23:10:13 -0400

    gprimelte: Move nfc configs and packages down from common
    Change-Id: I43df2f8108e19a5cd623e3ecaca9255b6d07ff03

--- device.mk
+++ device.mk
@@ -2,7 +2,6 @@
 $(call inherit-product-if-exists, vendor/samsung/gprimeltetfnvzw/gprimeltetfnvzw-vendor.mk)
 
 # Inherit from common
-BOARD_NFC_CHIPSET := pn547
 $(call inherit-product, device/samsung/gprimelte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/gprimeltetfnvzw
@@ -16,4 +15,11 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/gprimeltetfnvzw/overlay
 # NFC
 NXP_CHIP_TYPE := 1
 PRODUCT_PACKAGES += \
+	libpn547_fw \
 	nfc_nci.pn54x.msm8916
+
+# NFC configs
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
+	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
