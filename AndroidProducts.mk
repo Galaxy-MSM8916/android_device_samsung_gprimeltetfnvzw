ec5505796340aa25b97a0e835c3c73027f0ea0b0
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-24 20:53:57 -0400

    gprimeltetfnvzw: Add AndroidProducts makefile
    Consider {lineage,rr,omni,pa}_gprimeltetfnvzw the AOSP way
    
    Change-Id: I20e33432b5ac9f472016dd905847c9d124a1c30b

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := device/samsung/gprimeltetfnvzw
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/rr_gprimeltetfnvzw.mk \
+	$(LOCAL_DIR)/pa_gprimeltetfnvzw.mk \
+	$(LOCAL_DIR)/lineage_gprimeltetfnvzw.mk
